2a4b7548856ba753228b17a94076afc53d1aa900
andyat91 <andres.alves.91@gmail.com>
2023-11-23 19:40:47 +0100

    creacion conexiones js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require(`express`);
 const app = express();
 
+
+//Crear carpeta estatica
 app.use(express.static(`public`));
+app.use(express.json());
+
+//Crear conexion con mysql:
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+ 
+    host: "localhost",
+    user: "root",
+    password: "volador25",
+    database: "proyectecommerce",
+})
 
 app.get(
     `/hello`,
@@ -9,6 +23,34 @@ app.get(
         response.send({message: "Hello World"})
     });
 
+
+connection.connect(function(error) {
+    if (error) {
+        return console.error(`error: ${error.message}`);
+      }  
+    console.log("Conectado a MySQL!!") ;
+});
+
+
+//------------------------------------------------------------Endpoints para carrito---------------------
+//Desde carrito trae informacion de tres tablas unidas : compras - compraproducto - productos
+app.get(`/compras/:id`, function(request,response) {
+    
+    connection.query(
+        `select * from productos`, function(error,result,fields) {
+            if (error) {
+                response.status(400).send(`error ${error.message}`);
+                return;
+              }
+              console.log(result);
+              response.send(result);
+        });
+    
+    });
+
+//--------------------------TERMINA ENDPOINTS CARRITO--------------------------------------------------------
+
 app.listen(8000, function () {
     console.log("API up and running")
 });
+    
